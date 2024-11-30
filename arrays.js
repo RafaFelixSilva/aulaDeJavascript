c8d942ba950d3e0ac12ac5278c609db193e75814
RafaFelixSilva <165770069+RafaFelixSilva@users.noreply.github.com>
2024-11-30 13:19:15 +0000

    tabuada e numeros pares

--- arrays.js
+++ arrays.js
@@ -15,3 +15,21 @@ for (let i = 0; i < notas.length; i++) {
 
 const media = soma / notas.length;
 console.log(media);
+
+// crie um programa que dado um numero imprima a sua tabuada.
+const numero = 8;
+for (let i = 1; i <= 10; i++) {
+    console.log(i * numero);
+}
+
+// crie um programa que seja capaz de percorrer uma lista de numeros e imprima cada numero par encontrado.
+
+const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < numeros.length; i++) {
+    const numero = numeros[i];
+    if(numero % 2 === 0) {
+        console.log(numero);
+    }
+    
+}
